db387be08eaa42f42a749c91e232c27260319a05
Eli Ribble <eli@theribbles.org>
2025-08-24 16:55:00 -0700

    Get a URL for doing login
    It's not quite right since we want to display a code, but it does
    actually redirect to the right place

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
 
 type CloudreveClient struct {
@@ -108,6 +109,48 @@ func (client *CloudreveClient) Post(path string, payload map[string]interface{})
 	return nil
 }
 
+func (client *CloudreveClient) Put(path string, payload map[string]interface{}) ([]byte, error) {
+	u := client.base.JoinPath("/api/v4", path)
+	p, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	body := bytes.NewBuffer(p)
+	req, err := http.NewRequest(http.MethodPut, u.String(), body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	respBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("PUT response:", string(respBody))
+	return respBody, nil
+}
+
+func (client *CloudreveClient) PutSessionOpenid() (*SessionOpenidResponse, error) {
+	params := map[string]interface{}{"hint": nil, "linking": false, "provider": 2}
+	content, err := client.Put("/session/openid", params)
+	if err != nil {
+		return nil, fmt.Errorf("Failed PUT /session/openid: %w", err)
+	}
+
+	var resp SessionOpenidResponse
+	err = json.Unmarshal(content, &resp)
+	if err != nil {
+			return nil, fmt.Errorf("Failed to parse response: %w", err)
+	}
+	return &resp, nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please specify the root URL")
@@ -118,6 +161,7 @@ func main() {
 		log.Fatal("Failed to parse base URL", err)
 	}
 
+	/*
 	if len(os.Args) < 3 {
 		fmt.Println("Please specify an email")
 		os.Exit(1)
@@ -129,6 +173,7 @@ func main() {
 		os.Exit(1)
 	}
 	password := os.Args[3]
+	*/
 	client := NewCloudreveClient(urlBase)
 	fmt.Println("Attempting login at:", urlBase)
 	/*url := client.UrlWithQuery("/session/prepare", map[string]string{"email": email})
@@ -136,6 +181,30 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to prepare session", err)
 	}*/
+
+	/*
 	body := map[string]interface{}{"email": email, "password": password}
 	err = client.Post("/session/token", body)
+	*/
+	sessionData, err := client.PutSessionOpenid()
+	if err != nil {
+		log.Fatal("Failed to get session:", err)
+	}
+	converted := FixJSONEscaping(sessionData.Data)
+	fmt.Println("URL:", converted)
+
+}
+
+func FixJSONEscaping(content string) string {
+	content = strings.ReplaceAll(content, "\\u003c", "<")
+	content = strings.ReplaceAll(content, "\\u003e", ">")
+	content = strings.ReplaceAll(content, "\\u0026", "&")
+	return content
+}
+
+type SessionOpenidResponse struct {
+	Code int    `json:"code"`
+	Data string  `json:"data"`
+	Error *string  `json:"error"`
+	Msg string  `json:"msg"`
 }
